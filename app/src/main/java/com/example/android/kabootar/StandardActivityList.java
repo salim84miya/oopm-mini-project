18e5fb1ab273c1ef304719a16e97ad8c4a9fd508
salim <salimsaudagar84@gmail.com>
2022-10-09 12:08:14 +0530

    added branch and batch drop down menu and interchanged activity.

---
+++ app/src/main/java/com/example/android/kabootar/StandardActivityList.java
@@ -0,0 +1,31 @@
+package com.example.android.kabootar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StandardActivityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.standard_request_list);
+
+        View view1 =(View)findViewById(R.id.list_item_1);
+
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                standardList();
+            }
+        });
+    }
+    public void standardList(){
+        Intent intent = new Intent(this,StandardActivity.class);
+        startActivity(intent);
+    }
+
+}
