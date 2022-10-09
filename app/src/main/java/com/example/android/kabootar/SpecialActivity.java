0410308754d2064e72584e994cf9798279eb43f4
salim <salimsaudagar84@gmail.com>
2022-10-09 13:12:17 +0530

    added branch and batch drop down menu and interchanged activity.

--- app/src/main/java/com/example/android/kabootar/SpecialActivity.java
+++ app/src/main/java/com/example/android/kabootar/SpecialActivity.java
@@ -1,18 +1,55 @@
 package com.example.android.kabootar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class SpecialActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPageAdapter viewPageAdapter;
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_special);
 
+        tabLayout = findViewById(R.id.layout_tab);
+        viewPager2 = findViewById(R.id.view_pager);
+        viewPageAdapter = new ViewPageAdapter(this);
+        viewPager2.setAdapter(viewPageAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+           viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+               @Override
+               public void onPageSelected(int position) {
+                   super.onPageSelected(position);
+                   tabLayout.getTabAt(position).select();
+               }
+           });
     }
 }
