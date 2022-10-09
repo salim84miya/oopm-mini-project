2f5e1d17a8e924b117a8917ce8d68dfe9e0e5b2c
salim <salimsaudagar84@gmail.com>
2022-10-09 10:49:47 +0530

    oopm java mini project

--- app/src/main/java/com/example/android/kabootar/SpecialActivity.java
+++ app/src/main/java/com/example/android/kabootar/SpecialActivity.java
@@ -6,13 +6,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SpecialActivity extends AppCompatActivity {
+public class SpecialActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private Spinner branchNameSpinner;
+    private Spinner batchSpinner;
+    String branch;
     String name;
     EditText inputName;
     String letter;
@@ -23,6 +30,24 @@ public class SpecialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_special);
+
+        branchNameSpinner=findViewById(R.id.branch_spinner);
+        branchNameSpinner.setOnItemSelectedListener(this);
+        String []branchName = getResources().getStringArray(R.array.branch_name);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,branchName);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        branchNameSpinner.setAdapter(adapter);
+
+        batchSpinner=findViewById(R.id.branch_spinner2);
+        batchSpinner.setOnItemSelectedListener(this);
+        String []batchNo = getResources().getStringArray(R.array.batch_no);
+        ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item,batchNo);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        batchSpinner.setAdapter(adapter1);
+
+
     }
     public void display(){
         TextView view =(TextView) findViewById(R.id.description);
@@ -56,7 +81,7 @@ public class SpecialActivity extends AppCompatActivity {
     }
 
     public String Letterformat(){
-        inputName = (EditText) findViewById(R.id.editTextTextPersonName);
+        inputName = (EditText) findViewById(R.id.edit_text_person_name);
         name= inputName.getText().toString();
 
         letter = "To,\n"+
@@ -69,7 +94,7 @@ public class SpecialActivity extends AppCompatActivity {
         "\n\nSubject: Request for College leaving certificate.\n"+
 
                 "\nRespected Sir / Madam,\n"+
-                "               With due respect, I would like to state that, I am "+name +" and was a student of your college batch number ____________ (Batch). I was a student of ____________ (Course / Department). I cleared with good marks cleared all my semester examinations.\n"+
+                "               With due respect, I would like to state that, I am "+name +" and was a student of your college batch number ____________ (Batch). I was a student of   "+branch+". I cleared with good marks cleared all my semester examinations.\n"+
                 "               As I’ve to get myself enrolled in another institute for further education I need to submit a college leaving certificate issued by you in my name. In order to prevent harm to my further studies, I need to deposit the certificate as soon as possible. Therefore, I request you to kindly issue a college leaving certificate at the earliest.\n"+
 
         "\n\n\nThanking you,\n"+
@@ -77,6 +102,19 @@ public class SpecialActivity extends AppCompatActivity {
         name+",\n";
 
         return letter;
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+     if(parent.getId() == R.id.branch_spinner ){
+      String valueFromSpinner = parent.getItemAtPosition(position).toString();
+      branch= valueFromSpinner;
+     }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
